a6db9a77a70edc27d08cf11cd364bfb6e289ecab
Enirate <enirate@gmail.com>
2017-03-19 00:42:26 +0100

    fixed homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(private router:Router) {}
+
+  goToAbout(){
+    this.router.navigateByUrl('about');
+  }
+
+  goToLogin(){
+    this.router.navigateByUrl('login');    
+  }
+
+  goToRecharge(){
+        this.router.navigateByUrl('recharge-card');
+  }
 }
+
